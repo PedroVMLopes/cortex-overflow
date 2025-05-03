15a4f25e897e033208361d075f72001807ca2ba4
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-05-03 18:48:17 -0300

    Project Start

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,19 @@
+export default function Navbar():any {
+
+    return (
+        <div className="grid grid-cols-4 grid-rows-1 gap-2">
+            <div>
+                <p>Daily Objectives</p>
+            </div>
+            <div>
+                <p>Mission Objectives</p>
+            </div>
+            <div>
+                <p>Store</p>
+            </div>
+            <div>
+                <p>Stats</p>
+            </div>
+        </div>
+    )
+}
