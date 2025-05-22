202755378c536d6feed7e6aefc1e248d8cc9efab
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-05-22 00:33:23 -0300

    Navbar rework

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Navbar():any {
 
     return (
-        <div className="fixed bottom-0 pb-1 px-1 w-full font-mono text-emerald-500">
-            <div className="grid grid-cols-3 items-center text-center m-2 py-1.5 border border-emerald-800 bg-emerald-950">
+        <div className="fixed bottom-0 pb-2 p-2 w-full font-mono text-emerald-500">
+            <div className="grid grid-cols-3 items-center text-center py-1.5 border bg-emerald-950 border-emerald-900">
                 <div className="flex flex-col justify-center items-center">
                     <div className="flex flex-row gap-1.5 items-center">
                         <GiCrownCoin className="text-gray-400"/>
@@ -31,18 +31,18 @@ export default function Navbar():any {
                 </div>
                     
             </div>
-            <div className="grid grid-cols-4 grid-rows-1 my-2 items-center text-center text-3xl">
+            <div className="grid grid-cols-4 grid-rows-1 items-center text-center text-2xl bg-emerald-950 border border-emerald-900 py-2">
                 <div className="flex justify-center">
-                    <Link href="/"><GiCyberEye className="bg-emerald-950 border border-emerald-900 p-0.5 shadow shadow-emerald-400" /></Link>
+                    <Link href="/"><GiCyberEye className="" /></Link>
                 </div>
                 <div className="flex justify-center">
-                    <Link href="/LegacyObjectives"><GiCrownedSkull className="bg-emerald-950 border border-emerald-900 p-0.5" /></Link>
+                    <Link href="/LegacyObjectives"><GiCrownedSkull className="" /></Link>
                 </div>
                 <div className="flex justify-center">
-                    <Link href="/Store"><GiOpenTreasureChest className="bg-emerald-950 border border-emerald-900 p-0.5" /></Link>
+                    <Link href="/Store"><GiOpenTreasureChest className="" /></Link>
                 </div>
                 <div className="flex justify-center">
-                    <button><GiCyborgFace className="bg-emerald-950 border border-emerald-900 p-0.5" /></button>
+                    <button><GiCyborgFace className="" /></button>
                 </div>
             </div>
         </div>
