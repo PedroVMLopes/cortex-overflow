b2bb94f1598702dd2643a191cdbd516f25b0c080
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-05-27 16:12:07 -0300

    Minor changes before supabase implementation

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import { GiCyborgFace, GiCyberEye, GiCrownedSkull, GiOpenTreasureChest, GiCrownCoin, GiGems } from "react-icons/gi";
 import Link from "next/link";
-import { useContext } from "react";
 import { useUser } from "../context/UserContext";
 
 export const Navbar = () => {
@@ -11,23 +10,23 @@ export const Navbar = () => {
 
     return (
         <div className="fixed bottom-0 w-full font-mono text-emerald-500">
-            <div className="grid grid-cols-3 gap-0.5 items-center text-center border bg-emerald-900 border-emerald-900">
+            <div className="grid grid-cols-3 gap-0.5 items-center text-center border border-b-0 border-emerald-900">
 
-                <div className="flex flex-col justify-center items-center bg-emerald-950 py-1">
+                <div className="flex flex-col justify-center items-center py-1">
                     <div className="flex flex-row gap-1.5 items-center">
                         <GiCrownCoin className="text-gray-400"/>
                         <p>{user.silver_amount}</p>
                     </div>
                 </div>
 
-                <div className="flex flex-col justify-center items-center bg-emerald-950 py-1">
+                <div className="flex flex-col justify-center items-center border-r border-l border-emerald-900 py-1">
                     <div className="flex flex-row gap-1.5 items-center">
                         <GiCrownCoin className="text-amber-300"/>
                         <p>{user.gold_amount}</p>
                     </div>
                 </div>
                     
-                <div className="flex flex-col justify-center items-center bg-emerald-950 py-1">
+                <div className="flex flex-col justify-center items-center py-1">
                     <div className="flex flex-row gap-1.5 items-center">
                         <GiGems className="text-blue-400"/>
                         <p>{user.gem_amount}</p>
@@ -35,9 +34,9 @@ export const Navbar = () => {
                 </div>
                     
             </div>
-            <div className="grid grid-cols-4 grid-rows-1 items-center text-center text-2xl bg-emerald-950 border border-emerald-900 py-2">
+            <div className="grid grid-cols-4 grid-rows-1 items-center text-center text-2xl border border-emerald-900 py-2">
                 <div className="flex justify-center">
-                    <Link href="/"><GiCyberEye className="" /></Link>
+                    <Link href="/"><GiCyberEye /></Link>
                 </div>
                 <div className="flex justify-center">
                     <Link href="/LegacyObjectives"><GiCrownedSkull /></Link>
