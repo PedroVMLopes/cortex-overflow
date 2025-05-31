2ac9358b959f55742a238625679dcdb14c7d3d88
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-05-31 17:45:42 -0300

    UI Tweaks

--- app/Store/page.tsx
+++ app/Store/page.tsx
@@ -13,7 +13,7 @@ export default function Store() {
             <div className="bg-black/70 backdrop-blur-lg flex flex-row items-center justify-between text-emerald-50 font-mono fixed w-full top-0 z-10">
                 <p className="flex flex-row items-center mb-1 px-2 py-1"> <FaAngleRight className="text-emerald-500" /> Biotech Supermarket </p>
             </div>
-            <div className="text-emerald-50 font-mono m-2 mt-12">
+            <div className="text-emerald-50 font-mono m-2 mt-16">
                 <CurrencyExchange />
                 <div className="h-10"></div>
                 <ChipStore />
--- components/CurrencyExchange.tsx
+++ components/CurrencyExchange.tsx
@@ -4,12 +4,12 @@ import { GiCrownCoin } from "react-icons/gi";
 export default function CurrencyExchange() {
     return (
         <>
-            <div className="pt-4">
+            <div className="">
                 <p className="bg-emerald-950 border-b text-emerald-500 font-bold p-1 text-center"> Transmutação_de_Moedas</p>
                 <div className="flex flex-row gap-4 p-4 bg-black/80">
 
                     {/* Silver to gold exchange */}
-                    <div className="flex flex-col items-center w-full bg-gradient-to-tl from-black to-amber-500/30 backdrop-blur-3xl">
+                    <div className="flex flex-col items-center w-full bg-gradient-to-b from-black to-emerald-500/20 backdrop-blur-3xl border border-emerald-950">
                         <div className="flex flex-col items-center justify-evenly w-full">
                             <div className="p-1 px-4 flex flex-col items-center w-20">
                                 <p className="flex flex-row items-center text-white"> <GiCrownCoin className="text-gray-300 text-2xl mr-1" /> 10 </p>
@@ -19,13 +19,13 @@ export default function CurrencyExchange() {
                                 <p className="flex flex-row items-center text-white"> <GiCrownCoin className="text-amber-400 text-2xl mr-1" /> 1 </p>
                             </div>
                         </div>
-                        <button className="bg-gradient-to-r from-emerald-900 to-emerald-700 border-4 border-b-2 border-r-2 border-emerald-950 w-[90%] cursor-pointer py-0.5 mb-2 font-bold shadow shadow-emerald-800 flex flex-row justify-center">
+                        <button className="bg-gradient-to-r from-emerald-900 to-emerald-500/60 border-l-6 border-b-4 border-r-4 border-t-2 border-emerald-950 w-[90%] cursor-pointer py-0.5 mb-2 font-bold shadow-md shadow-emerald-800 flex flex-row justify-center">
                             <p className="flex flex-row items-center text-emerald-50 text-base"> [EXEC] <GiCrownCoin className="text-gray-300 text-lg mx-1" /> 10 </p>
                         </button>
                     </div>
 
                     {/* Gold to gem exchange */}
-                    <div className="flex flex-col items-center w-full bg-gradient-to-tr from-black to-blue-500/30 backdrop-blur-3xl">
+                    <div className="flex flex-col items-center w-full bg-gradient-to-b from-black to-emerald-500/20 backdrop-blur-3xl border border-emerald-950">
                         <div className="flex flex-col items-center justify-evenly w-full">
                             <div className="p-1 px-4 flex flex-col items-center w-20">
                                 <p className="flex flex-row items-center text-white"> <GiCrownCoin className="text-amber-300 text-2xl mr-1" /> 5 </p>
@@ -35,7 +35,7 @@ export default function CurrencyExchange() {
                                 <p className="flex flex-row items-center text-white"> <FaGem className="text-cyan-400 mr-1" /> 1 </p>
                             </div>
                         </div>
-                        <button className="bg-gradient-to-r from-emerald-900 to-emerald-700 border-4 border-b-2 border-r-2 border-emerald-950 w-[90%] cursor-pointer py-0.5 mb-2 font-bold shadow shadow-emerald-800 flex flex-row justify-center">
+                        <button className="bg-gradient-to-r from-emerald-900 to-emerald-700/70 border-l-6 border-b-4 border-r-4 border-t-2 border-emerald-950 w-[90%] cursor-pointer py-0.5 mb-2 font-bold shadow-md shadow-emerald-800 flex flex-row justify-center">
                             <p className="flex flex-row items-center text-emerald-50 text-base"> [EXEC] <GiCrownCoin className="text-amber-300 text-lg mx-1" /> 1 </p>
                         </button>
                     </div>
