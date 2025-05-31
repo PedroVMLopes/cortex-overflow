6f862e7f5bab9cd79583c4a7fb170972a0cd7e42
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-05-31 00:49:37 -0300

    UI Changes on the store page

--- components/CurrencyExchange.tsx
+++ components/CurrencyExchange.tsx
@@ -5,10 +5,10 @@ export default function CurrencyExchange() {
     return (
         <>
             <div className="px-2">
-                <p className="font-bold mt-4 text-center text-emerald-50 backdrop-blur-2xl rounded"> TRANSMUTAÇÃO_DE_MOEDAS</p>
+                <p className="backdrop-blur bg-black/80 rounded p-1 text-center"> Transmutação de Moedas</p>
 
                 {/* Silver to gold exchange */}
-                <div className="flex flex-col items-center w-full mt-4 bg-gradient-to-r to-yellow-500/50 backdrop-blur-2xl rounded">
+                <div className="flex flex-col items-center w-full mt-4 bg-gradient-to-tr from-black/70 to-yellow-500/50 backdrop-blur-2xl rounded">
                     <div className="flex flex-row items-center justify-between w-full">
                         <div className="p-2 px-4 flex flex-col items-center w-20">
                             <p className="flex flex-row items-center text-white"> <GiCrownCoin className="text-gray-300 text-2xl mr-1" /> 10 </p>
@@ -20,13 +20,13 @@ export default function CurrencyExchange() {
                             <p className="text-sm">OURO</p>
                         </div>
                     </div>
+                    <button className="backdrop-blur bg-black w-[90%] cursor-pointer text-amber-300 py-0.5 mb-2 font-bold shadow-lg flex flex-row justify-center rounded">
+                        <p className="flex flex-row items-center text-emerald-50"> TRANSMUTAR <GiCrownCoin className="text-gray-300 text-lg mx-1" /> 10 </p>
+                    </button>
                 </div>
-                <button className="backdrop-blur bg-black/70 w-full cursor-pointer text-amber-300 rounded mt-0.5 py-0.5 font-bold shadow-lg flex flex-row justify-center">
-                    [    <p className="text-emerald-50 mx-2"> REALIZAR TROCA </p>   ]
-                </button>
 
                 {/* Gold to gem exchange */}
-                <div className="flex flex-col items-center w-full mt-6 bg-gradient-to-r to-blue-600/70 backdrop-blur-2xl rounded">
+                <div className="flex flex-col items-center w-full mt-6 bg-gradient-to-tr from-black/70 to-blue-600/70 backdrop-blur-2xl rounded">
                     <div className="flex flex-row items-center justify-between w-full">
                         <div className="p-2 px-4 flex flex-col items-center w-20">
                             <p className="flex flex-row items-center text-white"> <GiCrownCoin className="text-amber-300 text-2xl mr-1" /> 5 </p>
@@ -38,10 +38,10 @@ export default function CurrencyExchange() {
                             <p className="text-sm">GEMA</p>
                         </div>
                     </div>
+                    <button className="backdrop-blur bg-black w-[90%] cursor-pointer text-amber-300 py-0.5 mb-2 font-bold shadow-lg flex flex-row justify-center rounded">
+                        <p className="flex flex-row items-center text-emerald-50"> TRANSMUTAR <GiCrownCoin className="text-amber-300 text-lg mx-1" /> 1 </p>
+                    </button>
                 </div>
-                <button className="backdrop-blur bg-black/70 w-full cursor-pointer text-blue-400 rounded mt-0.5 py-0.5 font-bold shadow-lg flex flex-row justify-center">
-                    [ <p className="text-emerald-50 mx-2"> REALIZAR TROCA </p> ] 
-                </button>
             </div>
         </>
     )
