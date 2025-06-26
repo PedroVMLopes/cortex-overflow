14357569d85ee3181d4f457b5431bb7e9180c829
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-06-26 20:27:08 -0300

    Test implemented

---
+++ hooks/__tests__/useTasks.test.ts
@@ -0,0 +1,92 @@
+import { renderHook, act } from "@testing-library/react";
+import { useTasks } from "@/hooks/useTasks";
+import { deleteTask, fetchTasks } from "@/services/taskServices";
+import { useUserContext } from "@/context/UserContext";
+
+jest.mock("@/services/taskServices", () => ({
+    deleteTask: jest.fn(),
+    fetchTasks: jest.fn(),
+}));
+
+jest.mock("@/context/UserContext", () => ({
+    useUserContext: jest.fn(),
+}));
+
+jest.mock("@/hooks/useUserAttributes", () => ({
+    useUserAttributes: jest.fn().mockReturnValue({
+        getSingleAttributeFromUser: jest.fn(),
+        updateUserAttribute: jest.fn(),
+    }),
+}));
+
+describe("useTasks with useUserContext and fetchTasks mock", () => {
+    const mockUserData = { id: 123 };
+
+    beforeEach(() => {
+        (useUserContext as jest.Mock).mockReturnValue({
+            userData: mockUserData,
+            refreshUserData: jest.fn(),
+        });
+
+        (fetchTasks as jest.Mock).mockResolvedValue([]);
+    });
+
+    it("removes the task", async () => {
+        (deleteTask as jest.Mock).mockResolvedValueOnce(undefined);
+
+        const fixedDate = new Date("2025-06-26T23:15:35.000Z");
+
+        const tasksMock = [
+            {
+                id: 1,
+                name: "",
+                silver_reward: 0,
+                gold_reward: 0,
+                is_completed: false,
+                reward_given: false,
+                user_id: 0,
+                xp_reward: 0,
+                created_at: fixedDate
+            },
+            {
+                id: 2,
+                name: "",
+                silver_reward: 0,
+                gold_reward: 0,
+                is_completed: false,
+                reward_given: false,
+                user_id: 0,
+                xp_reward: 0,
+                created_at: fixedDate
+            },
+            {
+                id: 3,
+                name: "",
+                silver_reward: 0,
+                gold_reward: 0,
+                is_completed: false,
+                reward_given: false,
+                user_id: 0,
+                xp_reward: 0,
+                created_at: fixedDate
+            }
+        ];
+
+        const { result } = renderHook(() => useTasks());
+
+        await act(async () => {
+            await Promise.resolve();
+        });
+
+        act(() => {
+            result.current.setTasks(tasksMock);
+        });
+
+        await act(async () => {
+            await result.current.removeTaskById(2, mockUserData.id);
+        });
+
+        expect(deleteTask).toHaveBeenCalledWith(2, mockUserData.id);
+        expect(result.current.tasks).toEqual([tasksMock[0], tasksMock[2]]);
+    });
+});
